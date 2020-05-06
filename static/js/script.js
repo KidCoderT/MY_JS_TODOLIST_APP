ad537dcc9942bc090436db974703e7f1b69b4b81
Tejas <Tejas75O25@gmail.com>
2020-05-06 16:52:27 +0530

    Chenging the html and css a bit and adding the basic javascipt code

--- static/js/script.js
+++ static/js/script.js
@@ -1,2 +1,30 @@
-
 // This the the JS code for my TODO list App
+
+var data;
+var documentElements;
+
+data = {
+    'finished': [],
+    'not_finished': [],
+};
+
+documentElements = {
+    "textInputField": document.getElementById('text'),
+    "todoContainerField": document.getElementsByClassName('finished__todo__container'),
+    "finishedTodoContainerField": document.getElementsByClassName('unfinished__todo__container'),
+}
+
+function addTodoItem() {
+    text = getText();
+    data['not_finished'].push(text);
+    addNewTodoItem();
+};
+
+function getText() {
+    textFieldInput = documentElements['textInputField'].value;
+    documentElements['textInputField'].value = "";
+    return textFieldInput;
+};
+
+function addNewTodoItem() {};
+
